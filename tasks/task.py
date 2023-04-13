c26af77e947c53cef12597c496d289c4b1b227aa
Gruzin <gruzd.roman@inbox.ru>
2023-04-13 13:37:00 +0300

    Commit

--- tasks/task.py
+++ tasks/task.py
@@ -1,7 +1,11 @@
 from typing import List
 
 def split_by_index(s: str, indexes: List[int]) -> List[str]:
-    """
-    Add your code here or call it from here   
-    """
-    pass
+    new_s = [s[i:j] for i, j in zip([0] + indexes, indexes + [None])]
+    if '' in new_s:
+        new_s.pop()
+    return new_s
+
+print(split_by_index("pythoniscool,isn'tit?", [6, 8, 12, 13, 18]))
+print(split_by_index("no luck", [42]))
+
